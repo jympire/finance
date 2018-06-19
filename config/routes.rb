fe022d9477c3c467e05106d595ec37620d8dd9da
Jim <jympire@gmail.com>
2018-06-19 13:55:10 -0700

    add registrations controller and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,9 @@ Rails.application.routes.draw do
 
   get 'terms' , to: 'pages#terms'
 
-  devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {sign_in: 'login', sign_out: 'logout', edit: 'edit', sign_up: 'register'},
+             controllers: {registrations: 'registrations'}
   
 end
